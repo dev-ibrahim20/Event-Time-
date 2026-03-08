87fd905d6ccd547aabbeefcefbbbe82775d3ef7a
Ibrahem Al-ashqar <dev.ibrahem@dev.ibrahem.mohamed@gmail.com>
2026-03-09 00:07:21 +0200

    remove some code

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -13,7 +13,3 @@
 @yield('css')
 <!--- Style css -->
 <link href="{{URL::asset('assets/css-rtl/style.css')}}" rel="stylesheet">
-<!--- Dark-mode css -->
-<link href="{{URL::asset('assets/css-rtl/style-dark.css')}}" rel="stylesheet">
-<!---Skinmodes css-->
-<link href="{{URL::asset('assets/css-rtl/skin-modes.css')}}" rel="stylesheet">
